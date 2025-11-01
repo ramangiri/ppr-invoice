667faa0ab9f7fae507a25f5cb6212ae744bf78dc
Giridharan Raman <60056285+ramangiri@users.noreply.github.com>
2025-11-01 14:02:06 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ppr-invoice-v2";
+const CACHE_NAME = "ppr-invoice-v3";
 const FILES_TO_CACHE = ["/", "/index.html", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"];
 self.addEventListener("install", e => {e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));self.skipWaiting();});
 self.addEventListener("fetch", e => {e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));});
